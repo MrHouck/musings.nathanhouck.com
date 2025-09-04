55eeb77f2e874817fe771c64a38dabcafa2bf349
Nathan <nhouck51@gmail.com>
2025-09-04 13:03:30 -0400

    New Post + Small Fixes and QOL Improvements

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,22 +10,29 @@ export default function Home({ allPostsData }) {
         <Layout>
             <div className="pt-10 justify-center flex max-w-[1200px]">
                 <div className="w-full flex flex-col justify-center items-center">
-                    <div className="px-10 pb-10 self-start">
-                        <h1 className="text-3xl">Nathan's Musings</h1>
-                        <h2 className="text-md text-[var(--secondary)]">Welcome to the inside of my brain</h2>
+                    <div className="w-full flex flex-row justify-between">
+                        <div className="flex-col px-10 pb-10 self-start">
+                            <h1 className="text-3xl">Nathan's Musings</h1>
+                            <h2 className="text-md text-[var(--secondary)]">Welcome to the inside of my brain</h2>
+
+                        </div>
+                        <div className="self-stretch pb-10 flex flex-col items-center justify-center gap-2 md:flex-row md:gap-4">
+                            <a href="https://nathanhouck.com" className="text-sm h-38px px-11 py-2 bg-[var(--secondary-button)] rounded-lg text-[var(--secondary-text)] hover:underline mr-10">Back to Reality</a>
+                        </div>
                     </div>
+
                     <div className="flex flex-wrap gap-3 justify-center">
                         {allPostsData.map((post) => {
                             return (
-                            <PostPanel 
-                                key={post.id}
-                                id={post.id}
-                                title={post.title}
-                                description={post.description}
-                                date={post.date}
-                            />);
+                                <PostPanel
+                                    key={post.id}
+                                    id={post.id}
+                                    title={post.title}
+                                    description={post.description}
+                                    date={post['date-stylized'] ? post['date-stylized'] : post.date}
+                                />);
                         })}
-                        
+
                     </div>
                 </div>
 
